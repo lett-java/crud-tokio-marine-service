5ecd6f5bde834978f479d705aa186d0799cd9d35
Fabio Lettieri <fabiolettieri09@gmail.com>
2022-04-06 18:48:33 -0300

    Criação do projeto

---
+++ src/main/java/br/com/flettieri/tokiomarineservice/TokioMarineServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.flettieri.tokiomarineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokioMarineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokioMarineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flettieri/tokiomarineservice/TokioMarineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flettieri.tokiomarineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokioMarineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
